2f7e609fe10ffb5c313bd31cc6bafe9bd79cf0f4
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-14 18:50:43 +0300

    add task 1 & task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,8 +7,6 @@ import { galleryItems } from './gallery-items.js';
 //       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
 //     description: 'Hokkaido Flower',
 //   }
-
-console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href=${original}>
@@ -16,8 +14,29 @@ const markup = galleryItems.map(({ preview, original, description }) => `<li cla
       class="gallery__image"
       src=${preview}
       data-source=${original}
-      alt=${description}
+      alt='${description}'
     />
   </a>
 </li>`).join('');
-gallery.insertAdjacentHTML('beforeend', markup)
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.addEventListener('click', onClick);
+function onClick(event) {
+    event.preventDefault()
+    if (!event.target.classList.contains('gallery__image')){
+        return;
+    }
+    const currentImageOriginal = event.target.dataset.source
+    const instance = basicLightbox.create(`
+    <img src=${currentImageOriginal} width="800" height="600">
+`)
+    instance.show();
+    document.addEventListener('keydown', onEscape)
+    function onEscape(event) {
+    if (event.code === 'Escape') {
+        instance.close()
+        document.removeEventListener('keydown', onEscape)
+        }   
+    }
+}
+
+
