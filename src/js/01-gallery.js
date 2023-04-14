dd67e706bc1ae9351f0fca9b76fd6f3751963208
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-14 09:51:45 +0300

    add markup gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   }
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`).join('');
+gallery.insertAdjacentHTML('beforeend', markup)
